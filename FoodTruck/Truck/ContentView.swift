38bc77dd06678b94f9c2ea35d5fe1db831ebc5e5
Adrian <adri4silva@gmail.com>
2024-06-15 13:37:38 +0200

    Add correct content view

--- FoodTruck/Truck/ContentView.swift
+++ FoodTruck/Truck/ContentView.swift
@@ -8,10 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Model
+
+    @State var model = FoodTruckModel()
+    @State private var navigation: [NavigationPath] = []
+
     // MARK: - Body
 
     var body: some View {
-        Text("Hello World!")
+        NavigationStack(path: $navigation) {
+            ScrollView(.vertical) {
+                VStack(spacing: 16) {
+                    TruckHeader()
+                    
+                    Grid(horizontalSpacing: 12, verticalSpacing: 12) {
+                        TruckDonutsCard(
+                            donuts: Array(model.donuts.values), 
+                            groupText: "Donuts"
+                        )
+                        .onTapGesture {
+                            navigation.append(.donuts)
+                        }
+                        
+                        if !model.favouriteDonuts.isEmpty {
+                            TruckDonutsCard(
+                                donuts: Array(model.favouriteDonuts.values), 
+                                groupText: "Favourites"
+                            )
+                        }
+                    }
+                    .fixedSize(horizontal: false, vertical: true)
+                    .padding([.horizontal, .bottom], 16)
+                    .frame(maxWidth: 1200)
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) { 
+                    Button {
+                        model.isSettingsPresented = true
+                    } label: {
+                        Image(systemName: "gear")
+                            .resizable()
+                            .frame(width: 34, height: 34)
+                            .bold()
+                    }
+                    .frame(width: 44, height: 44)
+                    .padding(.trailing)
+                }
+            }
+            .navigationBarTitleDisplayMode(.large)
+            .navigationDestination(for: NavigationPath.self) { path in
+                switch path {
+                    case .donuts:
+                        DonutGallery(model: model)
+                }
+            }
+            .sheet(isPresented: $model.isSettingsPresented) {
+                SettingsView(model: model)
+            }
+        }
     }
 }
 
