5551dfedd80ec69a3b42a22cb77983fd34d61d3d
Adrian <adri4silva@gmail.com>
2024-06-15 08:17:37 +0200

    Initial state with truck header

---
+++ FoodTruck/FoodTruckApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTruckApp.swift
+//  FoodTruck
+//
+//  Created by Adrián Silva on 15/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodTruckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
