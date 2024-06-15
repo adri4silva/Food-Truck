495dc103ee7aa867d681d829b18dafe3662babbb
Adrian <adri4silva@gmail.com>
2024-06-15 13:33:43 +0200

    Add content view

---
+++ FoodTruck/Truck/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FoodTruck
+//
+//  Created by Adrián Silva on 15/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - Body
+
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
